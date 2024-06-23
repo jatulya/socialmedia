3917b8f5474fece53175c91013c3312bdde43aa2
J-Atulya <atulyajayakrishnan@gmail.com>
2024-06-23 23:58:28 +0530

    Added form inputs

--- src/_auth/forms/Signup.tsx
+++ src/_auth/forms/Signup.tsx
@@ -33,29 +33,93 @@ const Signup = () => {
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/books51.jpg" alt="logo"/>
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
-        <p>To use my social media page, u gotta give ur details brethren!!</p>
-      </div>
 
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>      
+        <p className="text-light-3 small-medium md:base-regular">To use my social media page, u gotta give ur details brethren!!</p>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="form w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input 
+                    type="text" 
+                    placeholder="name" 
+                    className="shad-input"
+                    {...field} 
+                  />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input 
+                    type="text" 
+                    placeholder="username" 
+                    className="shad-input"
+                    {...field} 
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input 
+                    type="" 
+                    placeholder="email" 
+                    className="shad-input"
+                    {...field} 
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input 
+                    type="password" 
+                    placeholder="password" 
+                    className="shad-input"
+                    {...field} 
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="shad-button_primary">Submit</Button>
+        </form>
+      </div>
     </Form>
   )
 }
