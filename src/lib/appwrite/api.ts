da5e7dde427a95f39f7f9e2ac23702e1a4160045
J-Atulya <atulyajayakrishnan@gmail.com>
2024-06-24 23:20:01 +0530

    added db basic

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,13 +1,26 @@
-import { INewUser, IUser } from "@/types/Interfaces";
+import { INewUser} from "@/types/Interfaces";
 import { ID } from "appwrite";
-import { account } from "./config";
+import { account, avatar } from "./config";
 
 export async function createUserAcc(user: INewUser ) {
     try{
         const newAccount = await account.create(ID.unique(), user.email, user.password, user.name)
+
+        if (!newAccount) throw Error
+        
+        const avatarUrl = avatar.getInitials(user.name)
+        const newUser = await saveUserToDB()
+
         return newAccount
     } catch(error){
         console.log(error)
         return error
     }
 }
+
+export async function saveUserToDB(user : {
+    
+}) {
+    
+}
+
