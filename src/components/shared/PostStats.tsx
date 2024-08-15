07da6b81ad2f6ac0b972cfba420711b7d0d93270
J-Atulya <atulyajayakrishnan@gmail.com>
2024-08-15 22:08:07 +0530

    poststats updated - not checked yet

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,5 @@
 import { useGetCurrUser } from "@/lib/react-query/queries"
+import { checkIsLiked } from "@/lib/utils"
 import { PostStatsProps } from "@/types/Interfaces"
 import { Models } from "appwrite"
 import { useState } from "react"
@@ -18,10 +19,62 @@ const PostStats = ({post, userId} : PostStatsProps) => {
     (record: Models.Document) => record.post.$id ===post.$id
   )
 
+  const handleLikePost = (e : React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    //<> event is of type mouse event on an image
+    e.stopPropagation() //only the specific event handler is triggered
+    
+    let likesarray = [...likes]
+
+    if (likesarray.includes(userId)){
+      likesarray = likesarray.filter((id) => id !== userId)
+      //if user already liked it, unlikes it
+    }else{
+      likesarray.push(userId) //user name added to likes list
+    }
+
+    setLikes(likesarray)
+    //likePost({postId: post.$id, likesarray})
+  }
+
+  const handleSavePost = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    e.stopPropagation()
+
+  }
+
+  const containerStyles = location.pathname.startsWith('/profile')
+  ? "w-full" : "";
+
   return (
-    <div>
+    <div
+      className={`flex justify-between items-center z-20 ${containerStyles}`}>
+      <div className="flex gap-2 mr-5">
+        <img
+          src={`${
+            checkIsLiked(likes, userId)
+              ? "/assets/icons/liked.svg"
+              : "/assets/icons/like.svg"
+          }`}
+          alt="like"
+          width={20}
+          height={20}
+          onClick={(e) => handleLikePost(e)}
+          className="cursor-pointer"
+        />
+        <p className="small-medium lg:base-medium">{likes.length}</p>
+      </div>
+
+      <div className="flex gap-2">
+        <img
+          src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
+          alt="share"
+          width={20}
+          height={20}
+          className="cursor-pointer"
+          onClick={(e) => handleSavePost(e)}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default PostStats
